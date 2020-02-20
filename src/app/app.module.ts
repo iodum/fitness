55f4be43a50ffd1895ca308c7c6c84bc5162b62a
Lavrenova <olavrenova@baltbet.ru>
2020-02-20 18:11:47 +0300

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TraningComponent } from './traning/traning.component';
+import { NewTrainingComponent } from './traning/new-training/new-training.component';
+import { CurrentTrainingComponent } from './traning/current-training/current-training.component';
+import { PastTrainingsComponent } from './traning/past-trainings/past-trainings.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TraningComponent,
+    NewTrainingComponent,
+    CurrentTrainingComponent,
+    PastTrainingsComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
